ae01db8c78fffdd9cf83b26b312c92542fae07c3
C.B <aschera@gmail.com>
2017-02-22 11:53:33 +0100

    Add files via upload

---
+++ page2_day1/wiki.js
@@ -0,0 +1,53 @@
+// wiki CLIENT ID : 1a4d5d1c55ad48baa5b9283de528ada8
+
+
+		window.addEventListener('load', function() {   // so shit loads and then runs.
+            
+            
+			let button = document.getElementById('button');
+			button.addEventListener('click', function() {
+                
+				// DOM-element to display results in.
+				
+				let res = document.getElementById('wiki_box');
+                		
+			
+// wikipedia api
+
+				let url3 = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=';
+
+				//city
+				url3 += result_country.city + '&';
+				
+
+				//rest
+				url3 += 'format=json&origin=*';
+
+                
+                //identification
+                //url3 += '1a4d5d1c55ad48baa5b9283de528ada8';
+                
+                
+                console.log (url3);
+                
+				// AJAX request Foursquare
+				let ajax3 = new XMLHttpRequest();
+				ajax3.open('get', url3);
+				ajax3.onreadystatechange = function() {
+					if( ajax3.status == 200 && ajax3.readyState == 4 ) {
+                        
+						// AJAX success
+						let json3= JSON.parse(ajax3.responseText);
+						
+						res.innerHTML = json3[2];
+						console.log(json3);
+                        
+                       
+					}
+					else if( ajax3.status != 200 ) {
+						console.log('error');
+					}
+				};
+				ajax3.send();
+});
+})
