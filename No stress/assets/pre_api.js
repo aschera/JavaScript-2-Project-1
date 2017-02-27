414cb63f6ee56242f42faba7fdc5c1ad046ea1ad
Gunnar Ahlbom <Gahlbom@gmail.com>
2017-02-27 12:15:19 +0100

    No stress, sexy stuff

---
+++ No stress/assets/pre_api.js
@@ -0,0 +1,599 @@
+
+let references = [
+	{
+		name:'Australia',
+		island:false,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Serbia',
+		island: false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Jamaica',
+		island: true,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Dense',
+		natural_disasters:true
+	},
+	{
+		name:'Panama',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Lithuania',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Cyprus',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Dense',
+		natural_disasters:true
+	},
+	{
+		name:'Macedonia',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarse',
+		natural_disasters:false
+	},
+	{
+		name:'Mauritius',
+		island:true,
+		excitement:'Wonders',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarse',
+		natural_disasters:true
+	},
+	{
+		name:'Tanzania',
+		island:true,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Paraguay',
+		island:false,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:true,
+		size:'Large',
+		temp:'Hot',
+		population_density:'dense',
+		natural_disasters:false
+	},
+	{
+		name:'Bulgaria',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Cuba',
+		island:true,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Dense',
+		natural_disasters:false
+	},
+	{
+		name:'Samoa',
+		island:true,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'United Arab Emirates',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Finland',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Jordan',
+		island:false,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Dense',
+		natural_disasters:false
+	},
+	{
+		name:'Slovenia',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Austria',
+		island:false,
+		excitement:'Wonders',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Namibia',
+		island:true,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Argentina',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Thailand',
+		island:true,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:true,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Dense',
+		natural_disasters:true
+	},
+	{
+		name:'South Korea',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Sierra Leone',
+		island:true,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Ukraine',
+		island:false,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Carge',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Ireland',
+		island:true,
+		excitement:'Wonders',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'United Kingdom',
+		island:true,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Liberia',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Mozambique',
+		island:false,
+		excitement:'Wonders',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:true,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Guinea-Bissau',
+		island:false,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Trinidad and Tobago',
+		island:true,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Chad',
+		island:false,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:true,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Tuvalu',
+		island:true,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Ecuador',
+		island:false,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Croatia',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Barbados',
+		island:true,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Norway',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Cold',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Venezuela',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		populaion_density:'Scarce',
+		natural_disasters:false,
+	},
+	{
+		name:'Montenegro',
+		island:false,
+		excitement:'Attractions',
+		safe:true,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Malta',
+		island:true,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Large',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Lesotho',
+		island:false,
+		excitement:'Attractions',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	},
+	{
+		name:'Israel',
+		island:false,
+		excitement:'Wonders',
+		safe:false,
+		dangerous_animals:false,
+		dangerous_diseases:false,
+		size:'Small',
+		temp:'Hot',
+		population_density:'Scarce',
+		natural_disasters:false
+	}
+];
+
+let userinput = [
+	{
+		island:null,
+		excitement:null,
+		safe:null,
+		dangerous_animals:null,
+		dangerous_diseases:null,
+		size:null,
+		temp:null,
+		population_density:null,
+		natural_disasters:null
+	}
+];
+
+let getID = id => document.getElementById(id);
+
+let current = 1;
+let obj = userinput[0];
+let questionAmount = 9;
+let currentInfo = getID('currentInfo');
+
+let q1  = getID('question1'),
+	q2  = getID('question2'),
+	q3  = getID('question3'),
+	q4  = getID('question4'),
+	q5  = getID('question5'),
+	q6  = getID('question6'),
+	q7  = getID('question7'),
+	q8  = getID('question8'),
+	q9  = getID('question9');
+
+let nextQuestion = (objectProperty, statement) => {
+	switch(current) {
+		case 1:  obj.             island = statement; hideDiv((q1));  currentInfo.innerHTML = 'Attractions or world wonders?';                        break;
+		case 2:  obj.         excitement = statement; hideDiv((q2));  currentInfo.innerHTML = 'Gangsters paradise or friendly officers?';    		  break;
+		case 3:  obj.               safe = statement; hideDiv((q3));  currentInfo.innerHTML = 'Dangerous or harmless animals?';              		  break;
+		case 4:  obj.  dangerous_animals = statement; hideDiv((q4));  currentInfo.innerHTML = 'Danger of diseases or harmless environment?'; 		  break;
+		case 5:  obj. dangerous_diseases = statement; hideDiv((q5));  currentInfo.innerHTML = 'Go on adventures or sit around and enjoy the moment?'; break;
+		case 6:  obj.               size = statement; hideDiv((q6));  currentInfo.innerHTML = 'Hot or Cold?';                                		  break;
+		case 7:  obj.               temp = statement; hideDiv((q7));  currentInfo.innerHTML = 'Lots of people or by yourself?';              		  break;
+		case 8:  obj. population_density = statement; hideDiv((q8));  currentInfo.innerHTML = 'Calm vibes or natural disasters?';            		  break;
+		case 9:  obj.  natural_disasters = statement; hideDiv((q9));  break;}
+	current++;
+	if(current > questionAmount) {
+		let result = compareObjectArrays(userinput,references);
+		sendAPIrequest(result);
+		hideDiv(getID('quiz-page'));
+		showDiv(getID('result-page'));}}
+
+let displayResult = name => {
+	let infoHeader = getID('resultHeader');
+	infoHeader.innerHTML = 'Congratulations! The quiz chose..';
+	let resultPlace = getID('resultResult');
+	resultPlace.innerHTML = name;}
+
+let sendAPIrequest = name => {
+	apiRequest(name);
+	displayResult(name)}
+
+let hideDiv = obj => {
+	obj.classList.remove('is-paused');
+	setTimeout(function(){ 
+		obj.style.display = 'none';
+		obj.style.visibility = 'hidden';
+		obj.style.opacity = '0';
+	},200);}
+
+let showDiv = obj => {
+	setTimeout(function () {
+		obj.style.visibility = 'visible';
+		obj.style.opacity = '1';
+	},200);}
+
+let compareObjectArrays = (a,b) => {
+	let result,
+		val = 0;
+	for(let x in a) {
+		for (let y in b) {
+			let sim = similarities(a[x], b[y]);
+			if(sim > val) {
+				val = sim;
+				result = b[y].name;}}}
+	return result;}
+
+let similarities = (input,reference) => {
+	let similarities = 0;
+	for (let object in reference) {
+		if(input[object] == reference[object]) {
+			similarities++;}}
+	return similarities;}
+
+let returnDataList = () => {return references;}
+
+let shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;}}
+
+shuffle(references);
